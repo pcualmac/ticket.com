af15a92101d1c07294806b0bb54be9aa1aec702c
Calogero Pumpignano <pumpignano@gmail.com>
2019-09-28 18:15:54 +0100

    add the rest

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Ticket</div>
+
+                    <div class="panel-body">
+                        <h1></h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Comments</div>
+
+                    <div class="panel-body">
+                        <h1></h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Client</div>
+
+                    <div class="panel-body">
+                        <h1></h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Categories</div>
+
+                    <div class="panel-body">
+                        <h1></h1>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
